088665211cbda8ba225ea5551c34a0144fdbd4cc
joao-riffel <joaovicenteriffel05@gmail.com>
2026-03-31 16:45:17 -0300

    Criadas ambas as rotas
    Criadas as rotas dos usuários e das tarefas

---
+++ backend/routes/tarefas.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const tarefasController = require("../controllers/tarefasController");
+
+router.get("/", tarefasController.listarTarefas);
+
+router.get("/:id", tarefasController.buscarTarefa);
+
+router.post("/", tarefasController.criarTarefa);
+
+router.put("/:id", tarefasController.atualizarTarefa);
+
+router.delete("/:id", tarefasController.deletarTarefa);
+
+module.exports = router;
